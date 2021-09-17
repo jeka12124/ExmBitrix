755c7d9d14d814c7ed513bc3f3a8418bf1082830
EvgeniyEP <epm@d-element.ru>
2021-09-17 10:25:19 +0500

    Add from local storage

---
+++ local/exam1/description.php
@@ -0,0 +1,8 @@
+<?
+$arTemplate = array (
+  'NAME' => 'Экзамен N1',
+  'DESCRIPTION' => 'Шаблон для теста ',
+  'SORT' => 1,
+  'TYPE' => '',
+);
+?>
---
+++ partneram/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества ", 
+		"/partneram/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнерам", 
+		"/partneram/kak-stat-partneram.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partneram/raspisanie-meropriyatiy/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
